dbaa8f10a60550d0bf2928bed59a49c2d4521c56
Karina <kar261183@gmail.com>
2020-02-14 07:38:33 -0800

    First automation

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/HomeworkTest.groovy
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox
+
+import org.testng.annotations.Test
+
+     class HomeworkTest {
+    @Test
+         void testDistance() {
+        Point point3 = new Point(34, 10)
+        Point point4 = new Point(3, 25)
+        double val = point3.distance(point4)
+        System.out.println("Расстояние между двумя точками: " + val)
+
+
+    }
+}
