c87d052aa393cb0e12389f17a51e3c864a75080d
Karina <kar261183@gmail.com>
2020-02-14 11:51:19 -0800

    Na proverky

--- sandbox/src/test/java/ru/stqa/pft/sandbox/HomeworkTest.groovy
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/HomeworkTest.groovy
@@ -2,9 +2,9 @@ package ru.stqa.pft.sandbox
 
 import org.testng.annotations.Test
 
-     class HomeworkTest {
+     public class HomeworkTest {
     @Test
-         void testDistance() {
+         public void testDistance() {
         Point point3 = new Point(34, 10)
         Point point4 = new Point(3, 25)
         double val = point3.distance(point4)
