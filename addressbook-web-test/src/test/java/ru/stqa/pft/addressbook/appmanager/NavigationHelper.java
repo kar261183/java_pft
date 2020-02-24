5dc84fe05bb1a0913d0afcacff6d3c85d18afcc2
Karina <kar261183@gmail.com>
2020-02-24 12:32:32 -0800

    LenivieDeistvia

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,6 +10,14 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.xpath("//h1[contains(text(),'Groups')]"))
+            && wd.findElement(By.xpath("//h1[contains(text(),'Groups')]")).getText().equals("Groups")
+            && isElementPresent(By.xpath("//body/div[@id='container']/div[@id='content']/form/input[1]"))) {
+      return;
+    }
     click(By.linkText("groups"));
+
   }
 }
+
+
