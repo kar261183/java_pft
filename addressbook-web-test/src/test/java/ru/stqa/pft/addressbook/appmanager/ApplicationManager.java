4a72c43dc9d7857afe5f66a48ec5f70ea4b66c79
Karina <kar261183@gmail.com>
2020-04-08 17:58:28 +0300

    HWConfigur

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,7 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver wd;
 
   private NameHelpers nameHelpers;
@@ -17,9 +23,12 @@ public class ApplicationManager {
 
   public ApplicationManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     if (browser.equals(BrowserType.CHROME)) {
       WebDriverManager.chromedriver().version("80.0.3987.106").setup();
       wd = new ChromeDriver();
@@ -27,12 +36,12 @@ public class ApplicationManager {
       WebDriverManager.firefoxdriver().setup();
       wd = new FirefoxDriver();
     }
-    wd.get("http://localhost:8080/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     nameHelpers = new NameHelpers(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
